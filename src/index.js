5cb67953589e7d6825a171212e1b9fe5fc3ec3fd
Jakub Kania <kubak.15@gmail.com>
2018-10-25 12:22:31 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "./styles.css";
 
@@ -6,10 +7,41 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   state = {
-    text: "aa"
+    results: [],
+    value: ""
   };
+
+  onChangeInput = e => {
+    this.setState({
+      valueInput: e.target.value
+    });
+  };
+
+  getResults = e => {
+    e.preventDefault();
+    const user = e.target.elements.valueInput.value;
+
+    axios.get(`https://images-api.nasa.gov/search?q=${user}`).then(res => {
+      console.log(res.data.collection.items[0].data[0]);
+      this.setState({
+        results: res.data.collection.items
+      });
+    });
+  };
+
   render() {
-    return <div className="App">{this.state.text}</div>;
+    return (
+      <div className="App">
+        <h2>Searching from API NASA</h2>
+        <form onSubmit={this.getResults}>
+          <input name="valueInput" />
+          <input type="submit" value="Submit" />
+        </form>
+        {this.state.results.map((e, i) => {
+          return <div key={i}>{e.data[0].title}</div>;
+        })}
+      </div>
+    );
   }
 }
 
